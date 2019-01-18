d55cc21c92da13e1a80f3258ef0f36166bf7326b
artur-auditore <defteros7@gmail.com>
2019-01-18 08:58:28 -0200

    primeiro commit

---
+++ app/src/main/java/com/example/artur/watch/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.artur.watch
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this, TimeLineActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
